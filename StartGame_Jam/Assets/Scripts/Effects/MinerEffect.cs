55e66979b59685e188f2cabce58bbd97fdcafba9
Mikov Denis <denis.mikov.06@gmail.com>
2023-09-30 22:46:36 +0300

    feat(effects): implemented miner

--- StartGame_Jam/Assets/Scripts/Effects/MinerEffect.cs
+++ StartGame_Jam/Assets/Scripts/Effects/MinerEffect.cs
@@ -1,13 +1,28 @@
-﻿using Player;
+﻿using System.Collections;
+using Player;
+using UnityEngine;
 using WorldGeneration;
 
 namespace Effects
 {
     public class MinerEffect : PlatformEffect
     {
+        [SerializeField] private float effectDuration;
+        [SerializeField] private float targetCameraZoomSize;
+
         public override void ExecuteOnPickUp(PlayerMovement player)
         {
-            throw new System.NotImplementedException();
+            var mainCamera = Camera.main;
+            var cameraStartSize = mainCamera.orthographicSize;
+            mainCamera.orthographicSize = targetCameraZoomSize;
+            StartCoroutine(WaitForSeconds());
+
+            IEnumerator WaitForSeconds()
+            {
+                yield return new WaitForSeconds(effectDuration);
+                mainCamera.orthographicSize = cameraStartSize;
+
+            }
         }
     }
 }
