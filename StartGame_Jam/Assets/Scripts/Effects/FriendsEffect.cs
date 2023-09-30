0e2f3e2ecf33f6b2f3aa824c2cb32d6e966ac937
Mikov Denis <denis.mikov.06@gmail.com>
2023-09-30 22:49:51 +0300

    feat(effects): implemented friends effect

--- StartGame_Jam/Assets/Scripts/Effects/FriendsEffect.cs
+++ StartGame_Jam/Assets/Scripts/Effects/FriendsEffect.cs
@@ -1,13 +1,24 @@
-﻿using Player;
+﻿using System.Collections;
+using Player;
+using UnityEngine;
 using WorldGeneration;
 
 namespace Effects
 {
     public class FriendsEffect : PlatformEffect
     {
+        [SerializeField] private float effectDuration;
+        [SerializeField] private int barrierRadius;
         public override void ExecuteOnPickUp(PlayerMovement player)
         {
-            throw new System.NotImplementedException();
+            // player.hacker.BarrierRadius = barrierRadius
+            StartCoroutine(WaitForDuration());
+
+            IEnumerator WaitForDuration()
+            {
+                yield return new WaitForSeconds(effectDuration);
+                // player.hacker.BarrierRadius = 0; 
+            }
         }
     }
 }
