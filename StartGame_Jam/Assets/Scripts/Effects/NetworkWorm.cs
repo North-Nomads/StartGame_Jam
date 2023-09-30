beed3f073d4c09bcc2e48cd24ecbde4b4738d36a
Mikov Denis <denis.mikov.06@gmail.com>
2023-09-30 22:34:09 +0300

    feat(effects): implemented network worm logic

--- StartGame_Jam/Assets/Scripts/Effects/NetworkWorm.cs
+++ StartGame_Jam/Assets/Scripts/Effects/NetworkWorm.cs
@@ -1,4 +1,5 @@
-﻿using Player;
+﻿using System.Collections;
+using Player;
 using UnityEngine;
 using WorldGeneration;
 
@@ -6,11 +7,29 @@ namespace Effects
 {
     public class NetworkWorm : PlatformEffect
     {
-        [SerializeField] private int reverseSteps;
+        [SerializeField] private int backStepsAmount;
+        [SerializeField] private float timeBetweenSteps;
         
         public override void ExecuteOnPickUp(PlayerMovement player)
         {
-            //
+            var queue = player.PlayerPath;
+            if (queue.Count == 0)
+                return;
+
+            StartCoroutine(PerformSteppingBack());
+
+            IEnumerator PerformSteppingBack()
+            {
+                for (int i = 0; i < backStepsAmount; i++)
+                {
+                    if (queue.Count == 0)
+                        yield break;
+
+                    var last = queue.Dequeue();
+                    // move player on last, check if he is got by hacker
+                    yield return new WaitForSeconds(timeBetweenSteps);
+                }
+            }
         }
     }
 }
