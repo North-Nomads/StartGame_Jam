2de0a05e933fbdfc8513246b35de49ecbd366daf
Mikov Denis <denis.mikov.06@gmail.com>
2023-09-30 22:40:00 +0300

    feat(effects): implemented ddos

--- StartGame_Jam/Assets/Scripts/Effects/DDOSEffect.cs
+++ StartGame_Jam/Assets/Scripts/Effects/DDOSEffect.cs
@@ -1,13 +1,24 @@
-﻿using Player;
+﻿using System.Collections;
+using Player;
+using UnityEngine;
 using WorldGeneration;
 
 namespace Effects
 {
     public class DDOSEffect : PlatformEffect
     {
+        [SerializeField] private float effectDuration;
+        
         public override void ExecuteOnPickUp(PlayerMovement player)
         {
-            throw new System.NotImplementedException();
+            // set player inversed bool -> True 
+            StartCoroutine(WaitForDuration());
+
+            IEnumerator WaitForDuration()
+            {
+                yield return new WaitForSeconds(effectDuration);
+                // set player inversed bool -> False
+            }
         }
     }
 }
