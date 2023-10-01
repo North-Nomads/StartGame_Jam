49d5291652532843f7616cb44da8d7cb15c7ca3e
Mikov Denis <denis.mikov.06@gmail.com>
2023-10-01 18:02:47 +0300

    feat(ui): implemented main menu

---
+++ StartGame_Jam/Assets/Scripts/Level/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Level
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+
+        public void LoadLevelsScene()
+        {
+            SceneManager.LoadScene(SceneIDs.LevelsSceneID);
+        }
+
+        public void PlayFirstLevel()
+        {
+            SceneManager.LoadScene(SceneIDs.LevelsSceneID + 1);
+        }
+    }
+}
